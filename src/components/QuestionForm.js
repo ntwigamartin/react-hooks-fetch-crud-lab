1643d020262b0d9fbc5d0323345f1440d6c229cb
ntwigamartin <ntwiga.martin@gmail.com>
2023-01-21 01:11:13 +0300

    completed

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
+  console.log(props);
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -20,6 +21,25 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+
+    fetch("http://localhost:4000/questions", {
+    method: "POST",    
+    headers: { "Content-Type": "application/json" },
+    body:JSON.stringify({
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    
+    })
+        
+      })
+      .then((response) => response.json())
+      .then((data) => setFormData(data))
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then((data) => setQuestions(data))
+  },[])
+  
+  function deleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((q) => q.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  function updateAnswer(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = questions.map((q) => {
+          if (q.id === updatedQuestion.id) return updatedQuestion;
+          return q;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question=> 
+      <QuestionItem 
+      question={question} 
+      key={question.id}
+      deleteQuestion={deleteQuestion}
+      updateAnswer={updateAnswer} />)}</ul>
     </section>
   );
 }
